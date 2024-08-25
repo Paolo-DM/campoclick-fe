d328410b9d36257301d9c3ad85856aabfbcbf8c6
Paolo Di Martino <paolo.dimartino@outlook.it>
2024-08-25 11:05:09 +0200

    fixed id used as key, added image credit

--- src/app/campi/[sport]/page.js
+++ src/app/campi/[sport]/page.js
@@ -1,6 +1,5 @@
 // Components
 import HeroImage from "@/components/ui/heroImage";
-import CardWithText from "@/components/ui/cardWithText";
 import CardSportCourt from "@/components/ui/cardSportCourt";
 
 // Utils
@@ -9,8 +8,8 @@ import { sportsSlugData } from "@/utils/properties";
 async function SportPage({ params }) {
   // Recupero i dati dei campi dal backend usando il parametro sport passato nell'URL
   const data = await fetch(
-    `${process.env.NEXT_PUBLIC_API_HOST}/courts?${params.sport}`,
-    { cache: "default" },
+    `${process.env.NEXT_PUBLIC_API_HOST}/courts?sport=${params.sport}`,
+    { cache: "force-cache" },
   ).then((response) => response.json());
 
   // Recupero i dati dello sport corrente dall'array esportato da properties, in base al parametro passato nell'URL
@@ -38,17 +37,25 @@ async function SportPage({ params }) {
           Scegli il campo, verifica la disponibilità e prenota
         </h1>
         <div className="flex flex-wrap justify-center gap-6">
-          {data.map((court) => (
-            <CardSportCourt
-              key={court.cour_id}
-              title={court.court_name}
-              surface={court.court_surface}
-              description={court.court_type}
-              imageSrc={court.image_url}
-              path={court.href}
-              courtId={court.court_id}
-            />
-          ))}
+          {/* Crea un componente CardSportCourt per ciascun campo sportivo presente nel database */}
+          {data.length > 0 ? (
+            data.map((court) => (
+              <CardSportCourt
+                key={court.court_id}
+                title={court.court_name}
+                surface={court.court_surface}
+                description={court.court_type}
+                imageSrc={court.image_url}
+                imageCredit={court.image_credit}
+                path={court.href}
+                courtId={court.court_id}
+              />
+            ))
+          ) : (
+            <p className="text-center text-xl font-bold text-black">
+              Non ci sono campi disponibili
+            </p>
+          )}
         </div>
       </section>
     </section>
