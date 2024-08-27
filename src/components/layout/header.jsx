2707838b10ce7d11df2b1e9c01d94f2de85040ad
Paolo Di Martino <paolo.dimartino@outlook.it>
2024-08-27 17:00:20 +0200

    changed lateral menu items

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -42,16 +42,24 @@ export default function Header() {
 
   const menuItems = [
     {
-      label: "I campi",
-      href: "/campi",
+      label: "Campi da Tennis",
+      href: "/campi/tennis",
+    },
+    {
+      label: "Campi da Padel",
+      href: "/campi/padel",
+    },
+    {
+      label: "Campi da Basket",
+      href: "/campi/basket",
+    },
+    {
+      label: "Campi da Calcio",
+      href: "/campi/calcio",
     },
     {
       label: "FAQ",
       href: "/faq",
-    },
-    {
-      label: "Contatti",
-      href: "/contatti",
     },
   ];
 
@@ -76,122 +84,115 @@ export default function Header() {
       </NavbarContent>
 
       <NavbarContent className="hidden gap-4 md:flex" justify="center">
-        <NavbarItem>
-          <Dropdown>
-            <NavbarItem>
-              <DropdownTrigger>
-                <Button
-                  disableRipple
-                  className="bg-transparent p-0 data-[hover=true]:bg-transparent"
-                  endContent={
-                    <svg
-                      fill="none"
-                      height={24}
-                      viewBox="0 0 24 24"
-                      width={24}
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
-                        fill="white"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeMiterlimit={10}
-                        strokeWidth={1.5}
-                      />
-                    </svg>
-                  }
-                  radius="sm"
-                  variant="dark"
-                >
-                  I campi
-                </Button>
-              </DropdownTrigger>
-            </NavbarItem>
-            <DropdownMenu
-              color="primary"
-              aria-label="I campi"
-              itemClasses={{
-                base: "gap-4",
-              }}
+        <Dropdown>
+          <NavbarItem>
+            <DropdownTrigger>
+              <Button
+                disableRipple
+                className="bg-transparent p-0 data-[hover=true]:bg-transparent"
+                endContent={
+                  <svg
+                    fill="none"
+                    height={24}
+                    viewBox="0 0 24 24"
+                    width={24}
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
+                      fill="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeMiterlimit={10}
+                      strokeWidth={1.5}
+                    />
+                  </svg>
+                }
+                radius="sm"
+                variant="dark"
+              >
+                I campi
+              </Button>
+            </DropdownTrigger>
+          </NavbarItem>
+          <DropdownMenu
+            color="primary"
+            aria-label="I campi"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            <DropdownItem
+              key="autoscaling"
+              onPress={goTo("/campi/tennis")}
+              startContent={
+                <Image
+                  src={tennisIcon}
+                  width={40}
+                  height={40}
+                  alt="Tennis icon"
+                />
+              }
             >
-              <DropdownItem
-                key="autoscaling"
-                onPress={goTo("/campi/tennis")}
-                startContent={
-                  <Image
-                    src={tennisIcon}
-                    width={40}
-                    height={40}
-                    alt="Tennis icon"
-                  />
-                }
-              >
-                Tennis
-              </DropdownItem>
-              <DropdownItem
-                key="autoscaling"
-                onPress={goTo("/campi/padel")}
-                startContent={
-                  <Image src={padelIcon} width={40} height={40} alt="Padel icon" />
-                }
-              >
-                Padel
-              </DropdownItem>
-              <DropdownItem
-                key="autoscaling"
-                onPress={goTo("/campi/basket")}
-                startContent={
-                  <Image
-                    src={basketballIcon}
-                    width={40}
-                    height={40}
-                    alt="Basketball icon"
-                  />
-                }
-              >
-                Basket
-              </DropdownItem>
-              <DropdownItem
-                key="autoscaling"
-                onPress={goTo("/campi/calcio")}
-                startContent={
-                  <Image
-                    src={footballIcon}
-                    width={40}
-                    height={40}
-                    alt="Football icon"
-                  />
-                }
-              >
-                Calcio
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
-        </NavbarItem>
+              Tennis
+            </DropdownItem>
+            <DropdownItem
+              key="autoscaling"
+              onPress={goTo("/campi/padel")}
+              startContent={
+                <Image
+                  src={padelIcon}
+                  width={40}
+                  height={40}
+                  alt="Padel icon"
+                />
+              }
+            >
+              Padel
+            </DropdownItem>
+            <DropdownItem
+              key="autoscaling"
+              onPress={goTo("/campi/basket")}
+              startContent={
+                <Image
+                  src={basketballIcon}
+                  width={40}
+                  height={40}
+                  alt="Basketball icon"
+                />
+              }
+            >
+              Basket
+            </DropdownItem>
+            <DropdownItem
+              key="autoscaling"
+              onPress={goTo("/campi/calcio")}
+              startContent={
+                <Image
+                  src={footballIcon}
+                  width={40}
+                  height={40}
+                  alt="Football icon"
+                />
+              }
+            >
+              Calcio
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
         <NavbarItem>
           <Link href="#" aria-current="page" className="text-inherit">
             FAQ
           </Link>
         </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="#" className="text-inherit">
-            Contatti
-          </Link>
-        </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="hidden md:flex">
-          <Link href="#" className="text-inherit">
-            Prenota
-          </Link>
-        </NavbarItem>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="text-inherit md:hidden"
         />
       </NavbarContent>
-      <NavbarMenu>
+      <NavbarMenu className="gap-8 bg-myTertiary/60 pt-6 font-bold text-myQuaternary">
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item}-${index}`}>
             <Link
@@ -203,10 +204,10 @@ export default function Header() {
                     : "foreground"
               }
               className="w-full text-inherit"
-              href="#"
+              href={item.href}
               size="lg"
             >
-              {item}
+              {item.label}
             </Link>
           </NavbarMenuItem>
         ))}
